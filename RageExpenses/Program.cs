616c06f1fbddfa50505cc89baca5df50b86b8f75
Konstantin Kostenski <konstantin_kostenski@yahoo.com>
2019-02-20 08:15:17 +0200

    Next commit.

---
+++ RageExpenses/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace RageExpenses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            decimal headsetPrice = decimal.Parse(Console.ReadLine());
+            decimal mousePrice = decimal.Parse(Console.ReadLine());
+            decimal keyboardPrice = decimal.Parse(Console.ReadLine());
+            decimal displayPrice = decimal.Parse(Console.ReadLine());
+            decimal totalExpenses = 0m;
+            int keyobardTrashTimes = 0;
+
+            for (int i = 0; i <= n; i++)
+            {
+                if (i % 2 == 0 && i != 0)
+                {
+                    totalExpenses += headsetPrice;
+
+                    if (i % 3 == 0)
+                    {
+                        totalExpenses += mousePrice + keyboardPrice;
+                        keyobardTrashTimes++;
+
+                        if (keyobardTrashTimes % 2 == 0)
+                        {
+                            totalExpenses += displayPrice;
+                        }
+                    }
+                }
+                else if (i % 3 == 0 && i != 0)
+                {
+                    totalExpenses += mousePrice;
+                }
+            }
+
+            Console.WriteLine($"Rage expenses: {totalExpenses:f2} lv.");
+        }
+    }
+}
